3d2834c59390cff1410e2de1cad89ab29201bd35
Carlos Raymundo <80983254+codetree4u@users.noreply.github.com>
2022-11-15 12:22:15 -0800

    Create AccessMethodOfTheBaseClass.py

---
+++ AccessMethodOfTheBaseClass.py
@@ -0,0 +1,20 @@
+"""
+Challenge: Access Method of the Base Class
+Easy
+Problem Description
+Create a program to call the method of the base class using an object of the derived class.
+
+Create classes
+
+Create a base class named Animal. Inside the class, create a method named eat() that prints 'I can eat food'.
+Create a Dog class that inherits the Animal class. Inside the class, create a method named bark() that prints 'I can bark'.
+Outside of classes
+
+Create an object of the Dog class.
+Call the eat() method using this object.
+Example
+Expected Output
+
+I can eat food
+
+"""
