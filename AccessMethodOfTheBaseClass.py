90607594105fef3eaf4913e5f55efb02dfe2fed6
Carlos Raymundo <80983254+codetree4u@users.noreply.github.com>
2022-11-15 12:22:32 -0800

    Update AccessMethodOfTheBaseClass.py

--- AccessMethodOfTheBaseClass.py
+++ AccessMethodOfTheBaseClass.py
@@ -18,3 +18,15 @@ Expected Output
 I can eat food
 
 """
+# create the Animal class
+class Animal:
+    def eat(self):
+        print('I can eat food')
+# create the Dog class
+class Dog(Animal):
+    def bark():
+        print('I can bark')
+# create an object of Dog
+obj1 = Dog()
+# call the eat() method using the object
+obj1.eat()
