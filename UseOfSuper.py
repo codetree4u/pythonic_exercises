34bbdff89e85d39ce68524bde5c8b3b3f6a95595
Carlos Raymundo <80983254+codetree4u@users.noreply.github.com>
2022-11-16 15:01:38 -0800

    Update UseOfSuper.py

--- UseOfSuper.py
+++ UseOfSuper.py
@@ -18,3 +18,16 @@ Expected Output
 I can eat food
 
 """
+# create the Animal class
+class Animal():
+    def eat(self):
+        print('I can eat food')
+# create the Dog class
+class Dog(Animal):
+    def bark(self):
+        print('I can bark')
+        super().__init__('Dog')
+# create an object of the Dog class
+obj1 = Dog()
+# call the eat() method using the object
+obj1.eat()
