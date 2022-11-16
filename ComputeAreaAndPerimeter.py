fb3008911e031ce80041f76521cca8ae4b5476bb
Carlos Raymundo <80983254+codetree4u@users.noreply.github.com>
2022-11-16 15:02:38 -0800

    Create ComputeAreaAndPerimeter.py

---
+++ ComputeAreaAndPerimeter.py
@@ -0,0 +1,30 @@
+"""
+Challenge: Compute Area and Perimeter
+Medium
+Problem Description
+Create a program to compute the area and perimeter of a square using class. If you don't know, a square is a rectangle with equal sides.
+
+Create a class
+
+Create a class named Square that has an attribute named length.
+Use __init__() to initialize the length attribute.
+Create the compute_area() method to compute the area of a square and return it.
+Create the compute_perimter() method to compute the perimeter of a square and return it.
+Outside the class
+
+Take integer input from the user and store it in the length variable.
+Create an object of the Square class by passing length as an argument.
+Call the compute_area() method and print the area.
+Call the compute_perimeter() method and print the perimeter.
+By the way, the area of a square is equal to length * length, and the perimeter of a square is equal to 4 * length.
+
+Example
+Test Input
+
+2
+Expected Output
+
+4
+8
+
+"""
