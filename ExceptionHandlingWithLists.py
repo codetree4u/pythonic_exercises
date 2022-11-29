190bd9ef1de14c6b6e1fe02248de07d0395ac4b8
Carlos Raymundo <80983254+codetree4u@users.noreply.github.com>
2022-11-29 09:34:07 -0800

    Update ExceptionHandlingWithLists.py

--- ExceptionHandlingWithLists.py
+++ ExceptionHandlingWithLists.py
@@ -16,3 +16,15 @@ Test Input
 10
 
 """
+# create a try block
+try:
+    cars = ['BMW', 'Ferrari', 'Audi', 'Tesla']
+
+    # take integer input
+    index = int(input())
+
+    # print the item from the cars list
+    print(cars[index])
+# create the except block
+except:
+    print("Wrong Index")
