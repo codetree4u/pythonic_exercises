b8dd6966190760e72ce69373cdcb05abd2b3e3cd
Carlos Raymundo <80983254+codetree4u@users.noreply.github.com>
2022-11-29 09:33:41 -0800

    Create ExceptionHandlingWithLists.py

---
+++ ExceptionHandlingWithLists.py
@@ -0,0 +1,18 @@
+"""
+Challenge: Exception Handling With Lists
+Easy
+Problem Description
+Create a program to print the item present at a given index. If the index is out of bounds, print Wrong index.
+
+Create a list of four strings: 'BMW', 'Ferrari', 'Audi', 'Tesla'.
+Take an integer input from the user and store it in the index variable.
+Print the item present at that index.
+However, if the index is out of bounds, instead of showing the default error, print Wrong Index.
+Note: Use exception handling to solve this problem.
+
+Example
+Test Input
+
+10
+
+"""
