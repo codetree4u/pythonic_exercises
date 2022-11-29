06839f713c32130edd11be7cc04ffb026076fde5
Carlos Raymundo <80983254+codetree4u@users.noreply.github.com>
2022-11-29 09:35:29 -0800

    Update ExceptionHandlingWithDictionary.py

--- ExceptionHandlingWithDictionary.py
+++ ExceptionHandlingWithDictionary.py
@@ -19,3 +19,15 @@ Expected Output
 Key is not available
 
 """
+# create a try block
+try:
+    countries = {'Nepal' : 'Kathmandu', 'Sweden' : 'Stockholm', 'Italy' : 'Rome'}
+
+    # take string input
+    key = input()
+
+    # print the value present at key
+    print(countries[key])
+# create the except block
+except:
+    print('Key is not available')
